ec318ef4db9556bbffc0f0dbd69e8e045e1755ae
irfanarafat16 <irfanbbt@gmail.com>
2022-04-11 02:31:09 +0600

    update for db

--- main.py
+++ main.py
@@ -10,21 +10,22 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
+print(f"db url: {os.environ.get('DATABASE_URL')}")
+print(f"s url: {os.environ.get('SECRET_KEY')}")
 login_manager = LoginManager()
 login_manager.init_app(app)
 
